e069e4ae99c175cadcd660020fd0a648f1edbba9
MechanicTC2 <sciencenatekwok@gmail.com>
2023-04-29 14:40:22 -0700

    remove pvnet stuff

--- test/modelsTest/userModelTest.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const userModel = require('../../models/userModel');
-
-describe('userModel', function(){
-   it('user model should return hello', function(){
-      assert.equal(userModel(), 'hello');
-   });
-});
