9aa81788400264ee1495703dc16739a13e8840ef
MechanicTC2 <sciencenatekwok@gmail.com>
2023-04-29 16:14:46 -0700

    add poke your mom

--- models/playModel.js
+++ models/playModel.js
@@ -1,7 +1,7 @@
 const pokemon = require('../pokemon');
 const pokemon_btype = require('../pokemon_type');
 
-async function calcDamage(id, lvl, crit, bpwr, atk, def) {
+async function calcDamage(id, lvl, crit, bpwr, atk, def, atktype) {
     let STAB = false;
     let type1 = pokemon_btype[id][0];
     let type2 = "bob";
