1fe036219e1d411fc2a3cff7d8871119e99ce92b
MechanicTC2 <sciencenatekwok@gmail.com>
2023-04-29 20:11:24 -0700

    type

--- models/playModel.js
+++ models/playModel.js
@@ -1,24 +1,45 @@
 const pokemon = require('../pokemon');
 const pokemon_btype = require('../pokemon_type');
+const type_chart = require('../type_chart');
 
-async function calcDamage(id, lvl, crit, bpwr, def, atktype) {
+async function calcDamage(id, id2, crit, bpwr, atktype) {
     let type1 = pokemon_btype[id][0];
     let type2 = "bob";
+    let type3 = "said";
     let STAB = 1;
-    console.log(pokemon_btype[id]);
     try {
         type2 = pokemon_btype[id][1];
+        type3 = pokemon_btype[id2][1];
     }
     catch (err) {
     }
     if (type1 == atktype || type2 == atktype) {
         STAB = 1.5;
     }
-    let dmg = (((2 * lvl * crit) / 5) + 2) * bpwr * (pokemon[id].special_defense / def);
+    let dmg = (((2 * 100 * crit) / 5) + 2) * bpwr * (pokemon[id].special_defense / pokemon[id2].special_defense);
     dmg /= 50;
     dmg += 2;
-    dmg *= STAB * 1 * 1 * ((Math.random() * (255 - 217) + 217) / 255);
+    console.log(isEffective(atktype, type3))
+    dmg *= STAB * isEffective(atktype, pokemon_btype[id2][0]) * isEffective(atktype, type3) * ((Math.random() * (255 - 217) + 217) / 255);
     return Math.floor(dmg);
 }
 
-module.exports = {calcDamage};
+async function isEffective(atktype, deftype) {
+	for (let i = 0; i < type_chart[atktype].effective.length; i++) {
+		if (type_chart[atktype].effective[i] == deftype) {
+			//checks if super effective
+			return 2;
+		}
+		else if (type_chart[atktype].resist[i] == deftype) {
+			return 0.5;
+		}
+		else {
+		}
+  	}
+	return 1;
+}
+
+async function move(pokemon1, pokemon2, move1, move2) {
+    
+}
+module.exports = {calcDamage, isEffective};
