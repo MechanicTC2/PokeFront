b3bfda5f2c40207fc2277c1468b7d134cf132859
joucode <joucode@gmail.com>
2023-04-29 17:48:15 -0700

    Update aiShit.js

--- models/aiShit.js
+++ models/aiShit.js
@@ -82,8 +82,9 @@ async function help() {
     let mainguy = shitsotp[0];
     
     let hp = 0.25;
+    let switchchance = 0;
     let typeadvantage = new Boolean(false);
-    if((typeadvantage = false)&&(hp<=0.25)){
+    if((typeadvantage = false)&&(hp<=0.25)&&switchchance(math.random * 5)=0){
         for(let j = 0; j<2; i++)
             {
                  mainguy = shitsotp[Math.floor(Math.random() * 5]
